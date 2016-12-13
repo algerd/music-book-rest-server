80c53d27765ca75b0018f57f3e5637258d70c7fd
Alexander <algerd75@mail.ru>
2016-12-13 08:58:45 +0300

    1

--- src/main/java/ru/javafx/repository/GenreRepository.java
+++ src/main/java/ru/javafx/repository/GenreRepository.java
@@ -1,8 +1,13 @@
 
 package ru.javafx.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javafx.entity.Genre;
 
@@ -10,6 +15,11 @@ import ru.javafx.entity.Genre;
 @RepositoryRestResource(collectionResourceRel = "genres", path = "genres")
 public interface GenreRepository extends PagingAndSortingRepository<Genre, Long> {
     
-    Genre findByName(String name);
+    @RestResource(path = "by_name", rel = "by_name")
+    @Query("select a from Genre a where lower(a.name) like lower(concat(:search, '%')) ")
+    Page<Genre> searchByName(
+        @Param("search") String search,    
+        @Param("pageable") Pageable pageable
+    );
     
 }
