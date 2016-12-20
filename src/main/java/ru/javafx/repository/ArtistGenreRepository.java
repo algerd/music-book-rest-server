9d70365b8217654791c558cb997f49b20c8d3045
Alexander <algerd75@mail.ru>
2016-12-20 14:22:51 +0300

    1

--- src/main/java/ru/javafx/repository/ArtistGenreRepository.java
+++ src/main/java/ru/javafx/repository/ArtistGenreRepository.java
@@ -1,10 +1,13 @@
 
 package ru.javafx.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javafx.entity.ArtistGenre;
 
@@ -16,5 +19,22 @@ public interface ArtistGenreRepository extends PagingAndSortingRepository<Artist
     ArtistGenre findByIdArtistAndIdGenre(
         @Param("id_artist") Long id_artist, 
         @Param("id_genre") Long id_genre);
-  
+    
+    @RestResource(path = "by_genre", rel = "by_genre")
+    Page<ArtistGenre> findByGenre_Id(
+        @Param("id") Long id, 
+        @Param("pageable") Pageable pageable); 
+    
+    @RestResource(path = "by_artist", rel = "by_artist")
+    Page<ArtistGenre> findByArtist_Id(
+        @Param("id") Long id, 
+        @Param("pageable") Pageable pageable); 
+    
+    /*
+    @Query("select ag from ArtistGenre ag where "
+            + "lower(ag.Artists.name) like lower(concat(:search, '%')) "
+            + "and ag.Artists.rating >= :minrating and ag.Artists.rating <= :maxrating"
+            + "and ag.Genres.id = :id"
+    )
+    */
 }
