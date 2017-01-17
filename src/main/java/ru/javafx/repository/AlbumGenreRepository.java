a2487a09ae327ee8fe4af4a0119d4657d6c00def
Alexander <algerd75@mail.ru>
2017-01-17 10:34:35 +0300

    1

--- src/main/java/ru/javafx/repository/AlbumGenreRepository.java
+++ src/main/java/ru/javafx/repository/AlbumGenreRepository.java
@@ -1,19 +1,18 @@
 
 package ru.javafx.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javafx.entity.Album;
 import ru.javafx.entity.AlbumGenre;
+import ru.javafx.entity.Genre;
 
 @Transactional
 @RepositoryRestResource(collectionResourceRel = "album_genres", path = "album_genres")
 public interface AlbumGenreRepository extends PagingAndSortingRepository<AlbumGenre, Long> {
 
-    @Query(value = "select * from album_genre where id_album = :id_album and id_genre = :id_genre", nativeQuery = true)
-    AlbumGenre findByIdAlbumAndIdGenre(
-        @Param("id_album") Long id_album, 
-        @Param("id_genre") Long id_genre);
+    AlbumGenre findByAlbumAndGenre(@Param("album") Album album, @Param("genre") Genre genre);
+    
 }
