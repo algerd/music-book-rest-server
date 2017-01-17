9d9867c674cabe4e4d2787300e40c8f0c4541584
Alexander <algerd75@mail.ru>
2017-01-17 12:38:22 +0300

    1

--- src/main/java/ru/javafx/repository/ArtistGenreRepository.java
+++ src/main/java/ru/javafx/repository/ArtistGenreRepository.java
@@ -6,11 +6,15 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javafx.entity.Artist;
 import ru.javafx.entity.ArtistGenre;
+import ru.javafx.entity.Genre;
 
 @Transactional
 @RepositoryRestResource(collectionResourceRel = "artist_genres", path = "artist_genres")
 public interface ArtistGenreRepository extends PagingAndSortingRepository<ArtistGenre, Long> {
+    
+    ArtistGenre findByArtistAndGenre(@Param("artist") Artist artist, @Param("genre") Genre genre);
     
     @Query(value = "select * from artist_genre where id_artist = :id_artist and id_genre = :id_genre", nativeQuery = true)
     ArtistGenre findByIdArtistAndIdGenre(
