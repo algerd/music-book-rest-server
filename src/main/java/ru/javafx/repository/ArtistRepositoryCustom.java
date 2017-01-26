bb121886334bd5d5f3407ee6fb445ec8b6dde18a
Alexander <algerd75@mail.ru>
2017-01-26 21:41:49 +0300

    1

--- src/main/java/ru/javafx/repository/ArtistRepositoryCustom.java
+++ src/main/java/ru/javafx/repository/ArtistRepositoryCustom.java
@@ -1,6 +1,19 @@
 
 package ru.javafx.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import ru.javafx.entity.Artist;
+
+@RepositoryRestResource(collectionResourceRel = "custom", path = "custom")
 public interface ArtistRepositoryCustom {
+   
+    @RestResource(path = "custom", rel = "custom")
+    Page<Artist> search(
+            @Param("search") String search, 
+            @Param("pageable") Pageable pageable);
     
 }
--- src/main/java/ru/javafx/repository/ArtistRepositoryImpl.java
+++ src/main/java/ru/javafx/repository/ArtistRepositoryImpl.java
@@ -1,16 +1,32 @@
 
 package ru.javafx.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QueryDslRepositorySupport;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.javafx.entity.Artist;
 import ru.javafx.entity.QArtist;
 
+@RepositoryRestResource(collectionResourceRel = "custom", path = "custom")
 public class ArtistRepositoryImpl extends QueryDslRepositorySupport implements ArtistRepositoryCustom {
 
     private static final QArtist artist = QArtist.artist;
     
+    //????
+    //@Autowired
+    //ArtistRepository artistRepository;
+    
     public ArtistRepositoryImpl() {
         super(Artist.class);
+    }
+    
+    @RestResource(path = "custom", rel = "custom")
+    public Page<Artist> search(@Param("search") String search, @Param("pageable") Pageable pageable) {
+        return null;
     }
 
 }
