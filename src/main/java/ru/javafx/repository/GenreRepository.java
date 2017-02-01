5271acea912c376e3e3ff273f09823093489f368
Alexander <algerd75@mail.ru>
2017-02-01 15:01:08 +0300

    1

--- src/main/java/ru/javafx/repository/GenreRepository.java
+++ src/main/java/ru/javafx/repository/GenreRepository.java
@@ -2,9 +2,10 @@
 package ru.javafx.repository;
 
 import java.util.List;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -13,20 +14,24 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.javafx.entity.Album;
 import ru.javafx.entity.Artist;
 import ru.javafx.entity.Genre;
+import ru.javafx.entity.QGenre;
+import ru.javafx.repository.operators.NumberMultiValueBinding;
+import ru.javafx.repository.operators.StringMultiValueBinding;
 
 @Transactional
 @RepositoryRestResource(collectionResourceRel = "genres", path = "genres")
-public interface GenreRepository extends PagingAndSortingRepository<Genre, Long> {
+public interface GenreRepository extends 
+        PagingAndSortingRepository<Genre, Long>,
+        QueryDslPredicateExecutor<Genre>,
+        QuerydslBinderCustomizer<QGenre> {
     
-    Genre findByName(String name);
-    
-    @RestResource(path = "by_name", rel = "by_name")
-    @Query("select genre from Genre genre "
-            + "where lower(genre.name) like lower(concat(:search, '%')) ")
-    Page<Genre> searchByName(
-        @Param("search") String search,    
-        @Param("pageable") Pageable pageable
-    );
+    @Override
+    default void customize(QuerydslBindings bindings, QGenre genre) {
+        // Default pathes:
+        bindings.bind(String.class).all(new StringMultiValueBinding()); 
+        bindings.bind(Integer.class).all(new NumberMultiValueBinding<>());             
+        // Alias pathes:
+    }
 
     @RestResource(path = "by_artist", rel = "by_artist")
     @Query("select artistGenre.genre from ArtistGenre artistGenre "
