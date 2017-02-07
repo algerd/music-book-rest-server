791a3e20cc7b3dca13b7fca8a75aa90c6f0272e2
Alexander <algerd75@mail.ru>
2017-02-07 13:55:23 +0300

    1

--- src/main/java/ru/javafx/repository/MusicianRepository.java
+++ src/main/java/ru/javafx/repository/MusicianRepository.java
@@ -1,15 +1,36 @@
 
 package ru.javafx.repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javafx.entity.Musician;
+import ru.javafx.entity.QMusician;
+import ru.javafx.repository.operators.NumberMultiValueBinding;
+import ru.javafx.repository.operators.StringMultiValueBinding;
 
 @Transactional
 @RepositoryRestResource(collectionResourceRel = "musicians", path = "musicians")
-public interface MusicianRepository extends PagingAndSortingRepository<Musician, Long> {
+public interface MusicianRepository extends 
+        PagingAndSortingRepository<Musician, Long>,
+        QueryDslPredicateExecutor<Musician>,
+        QuerydslBinderCustomizer<QMusician> {
     
-    Musician findByName(String name);
-    
+    final static Logger LOGGER = LoggerFactory.getLogger(MusicianRepository.class);
+          
+    @Override
+    default void customize(QuerydslBindings bindings, QMusician musician) {
+        // Default pathes:
+        bindings.bind(String.class).all(new StringMultiValueBinding()); 
+        bindings.bind(Integer.class).all(new NumberMultiValueBinding<>());
+             
+        // Alias pathes:
+        bindings.bind(musician.musicianGenres.any().genre.id).as("genre.id").all(new NumberMultiValueBinding<>());
+    }
+       
 }
